0bd5c2cb918db9f31f12440e2ea07d297182e0d8
unknown <dungdt@smartosc.com>
2012-09-21 23:41:34 +0700

    test commit with username

--- src/Smart/TestBundle/Controller/DefaultController.php
+++ src/Smart/TestBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        var_dump('test commit');
         $this->get('doctrine.orm.entity_manager');
         die('dam tien dung');
         return $this->render('SmartTestBundle:Default:index.html.twig', array('name' => $name));
