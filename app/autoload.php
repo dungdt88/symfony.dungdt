a7535f7746e90d3cbd58bd6c7a2faec4279c643e
unknown <dungdt@smartosc.com>
2012-09-21 23:17:08 +0700

    just commit ...

--- app/autoload.php
+++ app/autoload.php
@@ -1,15 +1,21 @@
 <?php
-
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Component\ClassLoader\ApcUniversalClassLoader;
 
-$loader = require __DIR__.'/../vendor/autoload.php';
+if (function_exists('apc_store')) {
+    $loader = require __DIR__.'/../vendor/autoload.php';
+    $loader = new ApcUniversalClassLoader('sf212');
+    $loader->register(true);
+} else {
+    $loader = require __DIR__.'/../vendor/autoload.php';
+}
 
 // intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+// if (!function_exists('intl_get_error_code')) {
+//     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
-}
+//     $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
+// }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
